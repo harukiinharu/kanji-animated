2de6d23581cac86411fa2a6e4e67c733c2a7a4e0
春木Haruki <harukiinharu@gmail.com>
2025-04-13 14:06:32 +0800

    use vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   resolve: {
     alias: {
       '@': '/src',
